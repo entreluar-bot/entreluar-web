40ac51998fe0739048a0ced1440cb6fcd2e7cb99
Candy Franca <candypr0306@gmail.com>
2026-09-19 13:17:08 -0300

    fix: removed static export and added image upload vision features

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,1 @@
-import type { NextConfig } from "next"; const nextConfig: NextConfig = { output: "export", images: { unoptimized: true } }; export default nextConfig;
+﻿import type { NextConfig } from "next"; const nextConfig: NextConfig = { /* config options here */ images: { unoptimized: true } }; export default nextConfig;
